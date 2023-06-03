ffab60ca88f4ce05c382e846061892fddc87585b
jrcardarelli <jrcardarelli@wpi.edu>
2023-06-03 17:55:21 -0400

    test changes

--- apps/frontend/src/ServiceRequest.tsx
+++ apps/frontend/src/ServiceRequest.tsx
@@ -1,9 +1,16 @@
 function ServiceRequest() {
     return <>
-        <h1>Service Request</h1>
+        <h1>Computer Service</h1>
         <a href={`/`}>
             <button className={"button"}>Return Home</button>
         </a>
+        <text>
+            test component
+            <div>
+                test component 2
+            </div>
+        </text>
+
     </>;
 }
 
