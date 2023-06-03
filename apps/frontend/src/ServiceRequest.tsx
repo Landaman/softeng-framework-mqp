4f53f30e914dea8ee88a288c7c997ab45197f5b5
jrcardarelli <jrcardarelli@wpi.edu>
2023-06-03 19:24:07 -0400

    working editable field

--- apps/frontend/src/ServiceRequest.tsx
+++ apps/frontend/src/ServiceRequest.tsx
@@ -1,14 +1,25 @@
+import {ChangeEvent, useState} from 'react';
+
 function ServiceRequest() {
-  return (
+
+    const [inputText, setInputText] = useState("test prompt");
+
+    function handleInput(e: ChangeEvent<HTMLInputElement>) {
+        setInputText(e.target.value);
+    }
+
+    return (
     <>
       <h1>Computer Service</h1>
+        <div className="label-container">
+            <label>Location:</label>
+            <input value={inputText} onChange={handleInput} />
+        </div>
+        <div>
       <a href={`/`}>
         <button className={"button"}>Return Home</button>
       </a>
-      <text>
-        test component
-        <div>test component 2</div>
-      </text>
+        </div>
     </>
   );
 }
