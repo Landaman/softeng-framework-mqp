52893d8d821a5738c9ab1e22bb8b5b03f37e1d4f
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2023-06-03 18:53:34 -0400

    added a navigation bar

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import './App.css';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./Homepage.tsx";
 import ServiceRequest from "./ServiceRequest.tsx";
 import TestPage from "./TestPage.tsx";
 import NavBar from "./NavBar.tsx";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Homepage/>
-        },
-        {
-            path: "/ServiceRequest",
-            element: <ServiceRequest/>,
-        },
-        {
-            path: "/TestPage",
-            element: <TestPage/>,
-        },
-    ]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "/ServiceRequest",
+      element: <ServiceRequest />,
+    },
+    {
+      path: "/TestPage",
+      element: <TestPage />,
+    },
+  ]);
 
-    return(
-        <>
-            <NavBar />
-            <RouterProvider router={router}/>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
