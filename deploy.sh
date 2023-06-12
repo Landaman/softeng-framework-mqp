9ba2d361e74bff41e5a275a35eb58009447a2ad4
Ian W <49083526+Landaman@users.noreply.github.com>
2023-06-12 14:47:09 -0400

    Reduced prod image size by removing unecessary cache items and requiring only the compose and current system types be supported

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-(docker-compose -f docker-compose.prod.yaml down) || exit 0 # If it's not running, just ignore the stop instruction
-git pull
-(docker-compose -f docker-compose.prod.yaml up --build -d)
