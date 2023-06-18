6f99388f483beaa6ffe7ec84ebb6dee087d802ea
Ian W <49083526+Landaman@users.noreply.github.com>
2023-06-18 21:34:13 +0100

    Added service request

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./Homepage.tsx";
-import ServiceRequest from "./ServiceRequest.tsx";
+import ServiceRequest from "./routes/ServiceRequest.tsx";
 import Login from "./Login.tsx";
 import TestPage from "./routes/highScore.tsx";
 import NavBar from "./NavBar.tsx";
