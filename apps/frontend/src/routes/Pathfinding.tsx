72a74ad30a253f369eb20e539b57b21ea5ad0770
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2023-07-23 19:04:36 -0400

    user can select start and end node and the map pans and zooms.

--- apps/frontend/src/routes/Pathfinding.tsx
+++ apps/frontend/src/routes/Pathfinding.tsx
@@ -1,5 +1,4 @@
 import "./Pathfinding.css";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import {
   useLayoutEffect,
   useState,
@@ -10,10 +9,25 @@ import {
 import { Edge, Node } from "database";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
+import { MapEdge, MapNode } from "../MapComponents.ts";
 
 function Pathfinding() {
-  const [nodes, setNodes] = useState<Array<Node>>([]);
-  const [edges, setEdges] = useState<Array<Edge>>([]);
+  const [dataNodes, setDataNodes] = useState<Array<Node>>([]);
+  const [dataEdges, setDataEdges] = useState<Array<Edge>>([]);
+  const [displayMode, setDisplayMode] = useState<string>("Map");
+  const [mapNodes, setMapNodes] = useState<Array<MapNode>>([]);
+  const [mapEdges, setMapEdges] = useState<Array<MapEdge>>([]);
+  const [hoverNode, setHoverNode] = useState(-1);
+  const [canvasX, setCanvasX] = useState(0);
+  const [canvasY, setCanvasY] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+  const [origionalX, setOrigionalX] = useState(0);
+  const [origionalY, setOrigionalY] = useState(0);
+  const [scale, setScale] = useState(1);
+  const [mapMode, setMapMode] = useState("Pan");
+  const [startNode, setStartNode] = useState("");
+  const [endNode, setEndNode] = useState("");
   const { getAccessTokenSilently } = useAuth0();
   useEffect(() => {
     const get = async () => {
@@ -25,18 +39,18 @@ function Pathfinding() {
         })
         .then((response) => {
           console.log(response.data.length);
-          setNodes(response.data as Array<Node>);
+          setDataNodes(response.data as Array<Node>);
         });
 
       axios
-        .get<Edge[]>(`/api/node/${""}`, {
+        .get<Edge[]>(`/api/edge/${""}`, {
           headers: {
             Authorization: `Bearer ${await getAccessTokenSilently()}`,
           },
         })
         .then((response) => {
           console.log(response.data.length);
-          setEdges(response.data as Array<Edge>);
+          setDataEdges(response.data as Array<Edge>);
         });
     };
     get();
@@ -58,42 +72,188 @@ function Pathfinding() {
       .slice(0, -2);
 
     //scale the canvas
-    const canvasY = style_height * dpi;
-    const canvasX = style_width * dpi;
-    canvas.setAttribute("height", String(canvasY));
-    canvas.setAttribute("width", String(canvasX));
+    setCanvasY(style_height * dpi);
+    setCanvasX(style_width * dpi);
+    canvas.setAttribute("height", String(3400));
+    canvas.setAttribute("width", String(5000));
 
     context.imageSmoothingEnabled = false;
+    context.lineWidth = 4;
 
-    let oldX = 0;
-    let oldY = 0;
-    if (nodes.length > 0) {
-      context.fillRect(
-        nodes[0].xCoord * (canvasX / 5000) - 3,
-        nodes[0].yCoord * (canvasY / 3400) - 3,
-        6,
-        6
-      );
-      oldX = nodes[0].xCoord * (canvasX / 5000) - 3;
-      oldY = nodes[0].yCoord * (canvasY / 3400) - 3;
+    if (displayMode === "Path") {
+      let oldX = 0;
+      let oldY = 0;
+      if (dataNodes.length > 0) {
+        context.fillRect(
+          dataNodes[0].xCoord - 3,
+          dataNodes[0].yCoord - 3,
+          6,
+          6
+        );
+        oldX = dataNodes[0].xCoord - 3;
+        oldY = dataNodes[0].yCoord - 3;
+      }
+      context.beginPath();
+      for (let i = 1; i < dataNodes.length; i++) {
+        const n = dataNodes[i];
+        const scaleX = n.xCoord - 3;
+        const scaleY = n.yCoord - 3;
+        context.moveTo(oldX, oldY);
+        context.fillRect(scaleX, scaleY, 6, 6);
+        context.lineTo(scaleX, scaleY);
+        oldX = scaleX;
+        oldY = scaleY;
+      }
+      context.stroke();
+    } else {
+      // draw edges
+      for (let i = 0; i < mapEdges.length; i++) {
+        context.beginPath();
+        const edge = mapEdges[i];
+        context.moveTo(mapNodes[edge.index1].x1, mapNodes[edge.index1].y1);
+        context.lineTo(mapNodes[edge.index2].x1, mapNodes[edge.index2].y1);
+        context.stroke();
+        context.strokeStyle = "black";
+      }
+
+      // draw nodes
+      for (let i = 0; i < mapNodes.length; i++) {
+        const n = mapNodes[i];
+        if (i === hoverNode) {
+          context.fillStyle = "#00FF00";
+          context.fillRect(n.x1 - 10, n.y1 - 10, 20, 20);
+          context.fillStyle = "black";
+        } else if (n.node.nodeID === startNode) {
+          context.fillStyle = "#0000FF";
+          context.fillRect(n.x1 - 10, n.y1 - 10, 20, 20);
+          context.fillStyle = "black";
+        } else if (n.node.nodeID === endNode) {
+          context.fillStyle = "#FF0000";
+          context.fillRect(n.x1 - 10, n.y1 - 10, 20, 20);
+          context.fillStyle = "black";
+        } else {
+          context.fillRect(n.x1 - 10, n.y1 - 10, 20, 20);
+        }
+      }
     }
-    context.beginPath();
-    for (let i = 1; i < nodes.length; i++) {
-      const n = nodes[i];
-      const scaleX = n.xCoord * (canvasX / 5000) - 3;
-      const scaleY = n.yCoord * (canvasY / 3400) - 3;
-      context.moveTo(oldX, oldY);
-      context.fillRect(scaleX, scaleY, 6, 6);
-      // context.lineTo(scaleX, scaleY);
-      oldX = scaleX;
-      oldY = scaleY;
-    }
-    context.stroke();
     c.current = context;
-  });
+  }, [
+    displayMode,
+    dataNodes,
+    mapEdges,
+    mapNodes,
+    hoverNode,
+    startNode,
+    endNode,
+  ]);
 
   function drawPath() {
-    setEdges(edges);
+    setDataEdges(dataEdges);
+  }
+
+  function buildMap(floor: string) {
+    clearCanvas();
+    const tempNodes: Array<MapNode> = [];
+    for (let i = 0; i < dataNodes.length; i++) {
+      const node: Node = dataNodes[i];
+      if (node.floor === floor) {
+        const x1: number = node.xCoord - 3;
+        const y1: number = node.yCoord - 3;
+        const mn: MapNode = { x1, y1, node };
+        tempNodes.push(mn);
+      }
+    }
+    setMapNodes(tempNodes);
+
+    const tempEdges: Array<MapEdge> = [];
+    for (let i = 0; i < dataEdges.length; i++) {
+      const edge: Edge = dataEdges[i];
+      if (
+        edge.endNodeId.slice(0, floor.length) === floor &&
+        edge.startNodeId.slice(0, floor.length) === floor
+      ) {
+        const index1: number = getMapNodeIndex(edge.startNodeId, tempNodes);
+        const index2: number = getMapNodeIndex(edge.endNodeId, tempNodes);
+        const me: MapEdge = { index1, index2, edge };
+        tempEdges.push(me);
+      }
+    }
+    setMapEdges(tempEdges);
+  }
+
+  function getMapNodeIndex(nodeID: string, nodes: Array<MapNode>): number {
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].node.nodeID === nodeID) {
+        return i;
+      }
+    }
+    return 0;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const handleMouseDown = (event) => {
+    const { clientX, clientY } = event;
+    if (mapMode === "Pan") {
+      setOrigionalX(clientX - translateX);
+      setOrigionalY(clientY - translateY);
+    } else if (mapMode === "Start") {
+      const i = findNode(clientX, clientY);
+      if (i >= 0) {
+        setStartNode(mapNodes[i].node.nodeID);
+        setMapMode("Pan");
+      }
+    } else {
+      const i = findNode(clientX, clientY);
+      if (i >= 0) {
+        setEndNode(mapNodes[i].node.nodeID);
+        setMapMode("Pan");
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setOrigionalX(0);
+    setOrigionalY(0);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    setHoverNode(findNode(clientX, clientY));
+    if (origionalX != 0) {
+      setTranslateX(clientX - origionalX);
+      setTranslateY(clientY - origionalY);
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const handleWheel = (event) => {
+    if (event.deltaY > 0) {
+      setScale(scale / 1.1);
+    } else {
+      setScale(scale * 1.1);
+    }
+  };
+
+  function findNode(x: number, y: number) {
+    const adjX =
+      (((x - 510 - translateX) * 5000) / canvasX - 2500) / scale + 2500;
+    const adjY =
+      (((y - 110 - translateY) * 3400) / canvasY - 1700) / scale + 1700;
+    for (let i = 0; i < mapNodes.length; i++) {
+      if (
+        adjX - 12 < mapNodes[i].x1 &&
+        mapNodes[i].x1 < adjX + 12 &&
+        adjY - 12 < mapNodes[i].y1 &&
+        mapNodes[i].y1 < adjY + 12
+      ) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   const [floor, setfloor] = useState("pathfindingCanvas L1");
@@ -103,44 +263,84 @@ function Pathfinding() {
   }
   function FloorL1() {
     setfloor("pathfindingCanvas L1");
-    clearCanvas();
+    setDisplayMode("Map");
+    buildMap("L1");
   }
   function FloorL2() {
     setfloor("pathfindingCanvas L2");
-    clearCanvas();
+    setDisplayMode("Map");
+    buildMap("L2");
   }
   function Floor1() {
     setfloor("pathfindingCanvas one");
-    clearCanvas();
+    setDisplayMode("Map");
+    buildMap("1");
   }
   function Floor2() {
     setfloor("pathfindingCanvas two");
-    clearCanvas();
+    setDisplayMode("Map");
+    buildMap("2");
   }
   function Floor3() {
     setfloor("pathfindingCanvas three");
-    clearCanvas();
+    setDisplayMode("Map");
+    buildMap("3");
   }
   function clearCanvas() {
-    setNodes([]);
+    setMapNodes([]);
+    setMapEdges([]);
   }
+
+  const selectStartNode = () => {
+    setMapMode("Start");
+  };
+
+  const selectEndNode = () => {
+    setMapMode("End");
+  };
 
   return (
     <div className={"Pathfinding"}>
       <div className={"pathfinding-inputs"}>
         <h1>Get Directions</h1>
-        <input />
-        <input />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button onClick={selectStartNode}>start Node</button>
+          <p>{startNode}</p>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button onClick={selectEndNode}>end Node</button>
+          <p>{endNode}</p>
+        </div>
         <button onClick={drawPath} className={"pathfindingButton"}>
           Submit
         </button>
       </div>
       <div className={"mapdiv"}>
-        <TransformWrapper>
-          <TransformComponent>
-            <canvas className={floor} ref={canvasRef}></canvas>
-          </TransformComponent>
-        </TransformWrapper>
+        <div
+          style={{
+            overflow: "hidden",
+            display: "flex",
+            flexGrow: "1",
+            width: "100%",
+            height: "100px",
+          }}
+        >
+          <div
+            style={{
+              transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`,
+              display: "flex",
+            }}
+          >
+            <canvas
+              className={floor}
+              ref={canvasRef}
+              onMouseMove={handleMouseMove}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+              onWheel={handleWheel}
+            ></canvas>
+          </div>
+        </div>
         <div className={"buttondiv"}>
           <button onClick={groundFloor} className={"floorButton"}>
             Ground
