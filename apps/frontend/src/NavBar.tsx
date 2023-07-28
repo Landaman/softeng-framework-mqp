fd20dfb894fe2d3441a7f608dee738f6a4c39a8d
Ari Schechter <43043654+aschechter10@users.noreply.github.com>
2023-07-28 08:23:02 -0400

    UI updates

--- apps/frontend/src/NavBar.tsx
+++ apps/frontend/src/NavBar.tsx
@@ -1,17 +1,15 @@
 import "./NavBar.css";
-import { Dropdown } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import hospital from "./assets/Hospital_Logo.png";
+import hospital from "./assets/mgb-logo-color.png";
 
 function NavBar() {
   const location = useLocation();
 
   const {
     loginWithRedirect,
-    logout,
-    user,
     isAuthenticated,
     isLoading,
     getAccessTokenSilently,
@@ -44,62 +42,109 @@ function NavBar() {
   ]);
 
   return (
-    <div className={"navBar"}>
-      <img src={hospital} className={"hospitalLogo"} />
-      <div className={"linkdiv"}>
-        <a className={"navBarLink"} href={`/`}>
-          <p>Home</p>
-        </a>
-        <a className={"navBarLink"} href={`/service-requests/computer/create`}>
-          <p>Computer Service</p>
-        </a>
-        <a className={"navBarLink"} href={`/service-requests/sanitation`}>
-          <p>Sanitation Service</p>
-        </a>
-        <a className={"navBarLink"} href={`/high-score`}>
-          <p>Test Page</p>
-        </a>
-        <a className={"navBarLink"} href={`/pathfinding`}>
-          <p>Pathfinding</p>
-        </a>
-        <a className={"navBarLink"} href={`/MapEditor`}>
-          <p>Map Editor</p>
-        </a>
-      </div>
-      <Dropdown id="loginDropdown" className="navBarLink">
-        <Dropdown.Toggle variant="secondary" size="sm">
-          {!isLoading && isAuthenticated && user !== undefined
-            ? user.name
-            : "Log In"}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item
-            as="button"
-            onClick={async () => {
-              await loginWithRedirect({
-                appState: {
-                  returnTo: location.pathname,
-                },
-              });
-            }}
+    <Navbar expand="lg" className={"navbar"}>
+      <Navbar.Brand href="/">
+        <img alt="" src={hospital} className={"hospitalLogo"} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/" className={"navbar-link"}>
+            Home
+          </Nav.Link>
+          <Nav.Link
+            href={`/service-requests/computer/create`}
+            className={"navbar-link"}
           >
-            Login
-          </Dropdown.Item>
-          <Dropdown.Item
-            as="button"
-            onClick={() => {
-              logout({
-                logoutParams: {
-                  returnTo: window.location.origin,
-                },
-              });
-            }}
+            Computer Service
+          </Nav.Link>
+          <Nav.Link
+            href={`/service-requests/sanitation`}
+            className={"navbar-link"}
           >
-            Logout
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
+            Sanitation Service
+          </Nav.Link>
+          <Nav.Link href={`/pathfinding`} className={"navbar-link"}>
+            Pathfinding
+          </Nav.Link>
+          <Nav.Link href={`/mapeditor`} className={"navbar-link"}>
+            Map Editor
+          </Nav.Link>
+          <div style={{ width: "450px" }}></div>
+          <NavDropdown
+            title="Login"
+            id="basic-nav-dropdown"
+            className={"navbar-link"}
+          >
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    // <div className={"navBar"}>
+    //   <img src={hospital} className={"hospitalLogo"} />
+    //   <div className={"linkdiv"}>
+    //     <a className={"navBarLink"} href={`/`}>
+    //       <p>Home</p>
+    //     </a>
+    //     <a className={"navBarLink"} href={`/service-requests/computer/create`}>
+    //       <p>Computer Service</p>
+    //     </a>
+    //     <a className={"navBarLink"} href={`/service-requests/sanitation`}>
+    //       <p>Sanitation Service</p>
+    //     </a>
+    //     <a className={"navBarLink"} href={`/high-score`}>
+    //       <p>Test Page</p>
+    //     </a>
+    //     <a className={"navBarLink"} href={`/pathfinding`}>
+    //       <p>Pathfinding</p>
+    //     </a>
+    //     <a className={"navBarLink"} href={`/MapEditor`}>
+    //       <p>Map Editor</p>
+    //     </a>
+    //   </div>
+    //   <Dropdown id="loginDropdown" className="navBarLink">
+    //     <Dropdown.Toggle variant="secondary" size="sm">
+    //       {!isLoading && isAuthenticated && user !== undefined
+    //         ? user.name
+    //         : "Log In"}
+    //     </Dropdown.Toggle>
+    //     <Dropdown.Menu>
+    //       <Dropdown.Item
+    //         as="button"
+    //         onClick={async () => {
+    //           await loginWithRedirect({
+    //             appState: {
+    //               returnTo: location.pathname,
+    //             },
+    //           });
+    //         }}
+    //       >
+    //         Login
+    //       </Dropdown.Item>
+    //       <Dropdown.Item
+    //         as="button"
+    //         onClick={() => {
+    //           logout({
+    //             logoutParams: {
+    //               returnTo: window.location.origin,
+    //             },
+    //           });
+    //         }}
+    //       >
+    //         Logout
+    //       </Dropdown.Item>
+    //     </Dropdown.Menu>
+    //   </Dropdown>
+    // </div>
   );
 }
 
--- apps/frontend/src/routes/Homepage.tsx
+++ apps/frontend/src/routes/Homepage.tsx
@@ -1,23 +1,60 @@
 import "./Homepage.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "react-bootstrap";
+
 function Homepage() {
+  const { user } = useAuth0();
+
   return (
     <div className={"homepage"}>
-      <h1>Homepage</h1>
-      <a href={`/service-requests/computer/create`}>
-        <button className={"homepageButton"}>Computer Request</button>
-      </a>
-      <a href={`/service-requests/sanitation`}>
-        <button className={"homepageButton"}>Sanitation Request</button>
-      </a>
-      <a href={`/high-score`}>
-        <button className={"homepageButton"}>Test Page</button>
-      </a>
-      <a href={`/login`}>
-        <button className={"homepageButton"}>Login</button>
-      </a>
-      <a href={`/pathfinding`}>
-        <button className={"homepageButton"}>Pathfinding</button>
-      </a>
+      <p className={"heading-text homepage-header"}>
+        Welcome{" "}
+        <span style={{ color: "var(--primary)" }}>{user && user.name}</span>
+      </p>
+      <div className={"data-points"}>
+        <div className={"data-points-data"}>
+          <p className={"data-text-bold"}>13</p>
+          <p>Outgoing service requests</p>
+        </div>
+        <div className={"data-points-data"}>
+          <p className={"data-text-bold"}>2</p>
+          <p>Moves upcoming this week</p>
+        </div>
+        <div className={"data-points-data"}>
+          <p className={"data-text-bold"}>14</p>
+          <p>Tasks assigned to you</p>
+        </div>
+        <div className={"data-points-data"}>
+          <p className={"data-text-bold"}>27</p>
+          <p>Service requests completed this week</p>
+        </div>
+        <div className={"data-points-data"}>
+          <p className={"data-text-bold"}>3</p>
+          <p>Messages in your inbox</p>
+        </div>
+      </div>
+      <div className={"services"}>
+        <div className={"service-request-hub"}>
+          <p className={"heading-text"}>Service Request Hub</p>
+          <p className={"text-block"}>
+            Check out our service requests including Sanitation requests and
+            Computer requests
+          </p>
+          <Button variant="outline-light" className={"homepage-btn"}>
+            Computer Service Request
+          </Button>
+          <Button variant="outline-light" className={"homepage-btn"}>
+            Sanitation Service Request
+          </Button>
+        </div>
+        <div className={"pathfinding"}>
+          <p className={"heading-text"}>Pathfinding</p>
+          <p className={"text-block"}>
+            Visit our Pathfinding page to get directions around the hospital
+          </p>
+          <Button variant="primary">Pathfinding</Button>
+        </div>
+      </div>
     </div>
   );
 }
